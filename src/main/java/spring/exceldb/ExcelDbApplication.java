b870169350a6b53a34769280e4e39fbb7e1730e2
yewon-Noh <yewo2nn16@gmail.com>
2022-12-04 19:58:20 +0900

    first commit

---
+++ src/main/java/spring/exceldb/ExcelDbApplication.java
@@ -0,0 +1,13 @@
+package spring.exceldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/exceldb/ExcelDbApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.exceldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
